274b22e586bad36c4bba49105197531863ab42a0
kida21 <kidusp22@gmail.com>
2022-06-26 11:25:22 +0300

    csharp assignment 2
    assignment 2

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //int a = 10;
+            Console.WriteLine("read age");
+
+            String age = Console.ReadLine();
+            int num = int.Parse(age);
+
+            if (num == 10)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    Console.WriteLine("age is" + num);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("age is not equal to 10");
+            }
+
+
+
+            //int age=Convert.ToInt32(Console.ReadLine());
+            //int tid = int.Parse("10");
+        }
+    }
+}
